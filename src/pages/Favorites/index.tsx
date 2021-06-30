a1ef8fc8a7193c3310e25d2254074f18d32671fe
Pedro César <pedrocs378@gmail.com>
2021-06-30 14:09:19 -0300

    feat: adiciona listagem dos favoritos

--- src/pages/Favorites/index.tsx
+++ src/pages/Favorites/index.tsx
@@ -1,8 +1,14 @@
+
 import { Header } from "../../components/Header";
+import { CharacterCard } from "../../components/CharacterCard";
+
+import { useFavorites } from "../../hooks/useFavorites";
 
 import { Container, FavoriteList } from './styles'
 
 export function Favorites() {
+
+	const { favorites } = useFavorites()
 
 	return (
 		<Container>
@@ -10,7 +16,14 @@ export function Favorites() {
 
 			<main>
 				<FavoriteList>
-
+					{favorites.map(character => {
+						return (
+							<CharacterCard
+								key={character.id}
+								character={character}
+							/>
+						)
+					})}
 				</FavoriteList>
 			</main>
 		</Container>
